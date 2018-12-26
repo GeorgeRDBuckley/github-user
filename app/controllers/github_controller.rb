dd50d69284e301437c3e234e0f736aee6ef0f250
George Buckley <g.buckley@live.com>
2018-12-26 14:17:15 +0000

    set up favourite language method

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,4 +1,18 @@
 class GithubController < ApplicationController
   def search
+
+  end
+
+  private
+
+  def favourite_language
+    repositories(user).inject(Hash.new(0)) do |languages, repo|
+      languages[repo.language] += 1
+    end
+    languages
+  end
+
+  def repositories(user)
+    Octokit.repositories(user)
   end
 end

beba3057c97a395cc97538f7b839b31536bdf3b6
George Buckley <g.buckley@live.com>
2018-12-28 23:48:33 +0000

    better styling

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -2,7 +2,6 @@ class GithubController < ApplicationController
   def search
     if params[:user]
       @language = favourite_language(params[:user])
-      raise
     end
   end
 
@@ -15,14 +14,19 @@ class GithubController < ApplicationController
         language[repo.language] = 0 if language[repo.language].nil?
         language[repo.language] += 1
       end
+      @error = false
       language.max_by{|k,v| v}.first
     rescue Octokit::NotFound
+      @error = true
       "No user found"
     rescue Octokit::TooManyRequests
+      @error = true
       "Sorry, API limit has been reached"
     rescue Octokit::ClientError => e
+      @error = true
       "Sorry, unexpected client error: #{e.message}"
     rescue Octokit::ServerError => e
+      @error = true
       "Sorry, unexpected server error: #{e.message}"
     end
   end

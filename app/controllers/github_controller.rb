795d7ee2b0487c7e4407bc99e6df3d189bfdd5d5
George Buckley <g.buckley@live.com>
2018-12-28 23:08:09 +0000

    search page set up

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,15 +1,17 @@
 class GithubController < ApplicationController
   def search
-
+    if params[:user]
+      @language = favourite_language(params[:user])
+    end
   end
 
   private
 
-  def favourite_language
+  def favourite_language(user)
     repositories(user).inject(Hash.new(0)) do |languages, repo|
       languages[repo.language] += 1
     end
-    languages
+    languages.max_by{|k,v| v}
   end
 
   def repositories(user)

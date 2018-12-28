7e13673c31b6a0e212391fed702de438ae0b0e91
George Buckley <g.buckley@live.com>
2018-12-28 23:39:59 +0000

    better error handling

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -2,18 +2,29 @@ class GithubController < ApplicationController
   def search
     if params[:user]
       @language = favourite_language(params[:user])
+      raise
     end
   end
 
   private
 
   def favourite_language(user)
-    language = {}
-    repositories(user).each do |repo|
-      language[repo.language] = 0 if language[repo.language].nil?
-      language[repo.language] += 1
+    begin
+      language = {}
+      repositories(user).each do |repo|
+        language[repo.language] = 0 if language[repo.language].nil?
+        language[repo.language] += 1
+      end
+      language.max_by{|k,v| v}.first
+    rescue Octokit::NotFound
+      "No user found"
+    rescue Octokit::TooManyRequests
+      "Sorry, API limit has been reached"
+    rescue Octokit::ClientError => e
+      "Sorry, unexpected client error: #{e.message}"
+    rescue Octokit::ServerError => e
+      "Sorry, unexpected server error: #{e.message}"
     end
-    language.max_by{|k,v| v}.first
   end
 
   def repositories(user)
